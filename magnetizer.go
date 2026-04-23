8f5db40f0b119cad00a875ef090c8c32b6b24469
C13L99 <6krBy96tu0@gmail.com>
2026-04-23 13:01:07 -0600

    Build the magnet link and print

--- magnetizer.go
+++ magnetizer.go
@@ -4,4 +4,9 @@ func main() {
 	hash := flag.String("-hash", "", "The magnet hash you have")
 	flag.Parse()
 
+	if *hash != "" {
+		var magnet string
+		magnet = fmt.Sprintf("magnet:?xt=urn:btih:%s", *hash)
+		fmt.Println(magnet)
+	}
 }
